ae747cc3b62b32faac2c888fd4b12f7bf07cc9b1
Iann221 <vincentiannugroho@gmail.com>
2022-10-21 08:49:07 +0700

    Initial Commit

---
+++ PhillipsRecruitment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhillipsRecruitment
+//
+//  Created by Vincentius Ian Widi Nugroho on 21/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
